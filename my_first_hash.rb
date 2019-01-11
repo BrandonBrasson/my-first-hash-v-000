714e2783e9849d9208d4ed5c801484f1d660b27b
Brandon Brasson <rocksong622@gmail.com>
2019-01-11 02:57:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,14 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-    my_hash = {"name" => "Brandon", "age" => 21, "location" => "FL"}
+    my_hash = {["name" => "Brandon", "age" => 21, "location" => "FL"]}
+    puts my_hash[]
 end
 
 
 def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
+
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
