d0613af71bb32ea2cce0e954750ada52c97fd1c1
Brandon Brasson <rocksong622@gmail.com>
2019-01-11 03:05:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash(hash)
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-    Hash = {["name" => "Brandon", "age" => 21, "location" => "FL"]}
+  hash = {"name" => "Brandon"}
 end
 
 
